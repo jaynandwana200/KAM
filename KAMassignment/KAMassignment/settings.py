fa5bc0aa6755e71fd0ebf0d2d30e13b352251c46
Jay Nandwana <69632001+jaynandwana200@users.noreply.github.com>
2024-12-25 03:04:11 +0530

    Added Live MYSQL Database ( AWS )

--- KAMassignment/KAMassignment/settings.py
+++ KAMassignment/KAMassignment/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'KAMassignment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'KAMASSIGN',
+        'USER' : 'django',
+        'PASSWORD' : 'Lakshi2004',
+        'HOST' : 'kam.ch802a80abk3.ap-southeast-2.rds.amazonaws.com',
+        'PORT' : 3306,
     }
 }
 
